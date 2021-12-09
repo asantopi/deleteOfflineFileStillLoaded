ea6deb1f7962cfe68f0685ec3ef24f9d03c62079
asantopi <asantopi@tibco.com>
2021-12-09 12:11:55 +0100

    Fixed a logic bug to select file to delete

--- extractFileName.sh
+++ extractFileName.sh
@@ -7,26 +7,26 @@ my_dir="$(dirname "$0")"
 . "$my_dir/utilityFunctions.sh"
 
 filename=$1 # getting the filename from first argument
-count=1 # count the line of file to pass it as first argument in the progress bar function
+count=0 # count the line of file to pass it as first argument in the progress bar function
 totalLineOfFile=$(wc -l $1 | grep -o "[0-9]\+")  # count the line of file to pass it as second argument in the progress bar function
 fileNameToDelete=$(date +"%Y-%m-%d_%H%M%S")"_list_of_file_to_delete.txt" 
 while read line; do
-	ProgressBar $count $totalLineOfFile # draw the progress bar
 	((count++)) # increment the counter
-	filename_xml=$(echo $line | grep -oE '\bTIBCO_PRODUCTMODELOffline_[^\s<>]*?.xml\b')
-	if [ -n "$filename_xml" ]; then
-		# Find if in the next line there is the phrase "is published successfully"
+	ProgressBar $count $totalLineOfFile # draw the progress bar
+	
+	# Find if in current line if there is the phrase "is published successfully"
+	ips=$(echo $line | grep -oE '[^$\s]*is published successfully[^$\s]*') # ips variable stands for "is published successfully" :)
+	if [ -n "$ips" ]; then
 		read line
 		((count++)) # increment the counter becaouse we read a new line of file
-		ips=$(echo $line | grep -oE '[^$\s]*is published successfully[^$\s]*') # ips variable stands for "is published successfully" :)
-		if [ -n "$ips" ]; then
-			# If is present the phrase "is published successfully" the file name can go in the list of deletion files. Otherwise the file is not published correctly a will not go into the list of deletion files.
+		filename_xml=$(echo $line | grep -oE '\bTIBCO_PRODUCTMODELOffline_[^\s<>]*?.xml\b')
+		if [ -n "$filename_xml" ]; then
+			# If is present the filename it go in the list of deletion files. Otherwise the file is not published correctly a will not go into the list of deletion files.
 			echo $filename_xml >> $fileNameToDelete
 		fi
 	fi
-done < $filename
 
-echo " "
+done < $filename
 
 if test -f "$fileNameToDelete"; then
     echo "File generated: " $fileNameToDelete
